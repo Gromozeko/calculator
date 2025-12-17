47d0009c1fbc3b897c06f29f6c729d6c89670cba
Gromozeko <144881744+Gromozeko@users.noreply.github.com>
2025-12-17 15:30:44 +0300

    Delete calc_project directory

--- calc_project/calculator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.calculator_view, name='calculator'),
-    path('history/', views.history_view, name='history'),
-]
